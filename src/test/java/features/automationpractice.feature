d960ae269ffcda245cfbebab4c120db5f9e9ce20
Optic969 <optic969@tut.by>
2020-07-18 18:12:09 +0300

    Added Allure UPD3

--- src/test/java/features/automationpractice.feature
+++ src/test/java/features/automationpractice.feature
@@ -4,9 +4,9 @@ Feature: AutomationPracticeTests
   Scenario: Open AutomationPractice, choose random three items in each category and add to cart, check cart with items,
   delete one items, search deleted items, click on checkbox
 
-    Given open Automationpractice page and add fist product from woman category to cart
+    Given open AutomationPractice page and add fist product from woman category to cart
     When add second product to cart from dresses category
-    When add third product to cart from tshirt category
+    When add third product to cart from Tshirt category
     When check cart and delete one of products
     When search deleted products
     When click on checkbox
--- src/test/java/steps/AutomationPracticeSteps.java
+++ src/test/java/steps/AutomationPracticeSteps.java
@@ -16,15 +16,15 @@ public class AutomationPracticeSteps {
     Cart cart=new Cart();
     HomePage homePage= new HomePage();
 
-    @Given("open Automationpractice page and add fist product from woman category to cart")
-    public void openAutomationpracticePageAndAddFirstProductFromWomanCategoryToCart(){
+    @Given("open AutomationPractice page and add fist product from woman category to cart")
+    public void openAutomationPracticePageAndAddFirstProductFromWomanCategoryToCart(){
         woman.getWomenItem();
     }
     @When("add second product to cart from dresses category")
     public void addSecondProductToCartFromDressesCategory(){
         dresses.getDressesItem();
     }
-    @When("add third product to cart from tshirt category")
+    @When("add third product to cart from Tshirt category")
     public void addThirdProductToCartFromTshirtCategory(){
         tShirts.getTShirtsItem();
     }
